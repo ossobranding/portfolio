6b88c1e2edbc9d7f6806cd6fb744a0936ad398e6
Oscar <oscarcl.work@gmail.com>
2022-03-26 01:57:38 +0100

    skills respondDqweqwer

--- src/App.vue
+++ src/App.vue
@@ -13,98 +13,92 @@
         </v-btn-toggle>
       </v-row>
 
-      <v-row style="height: 300px">
-        <v-col>
-          <v-card class="fill-height"> Hola </v-card>
+      <v-row>
+        <v-col  
+          style="height: 300px; width:170px; min-height:300px; min-width:170px;">
+          
+          <v-card 
+            class="fill-height" 
+            style="background-color:#CCAB87;">  </v-card>
         </v-col>
 
-        <v-col class="pb-0">
+        <v-col 
+          class="pb-0" 
+          style="height: 300px; width:200px; min-height:300px; min-width:200px;">
+
           <v-row style="height: 50%">
             <v-col class="ma-0">
-              <v-card class="fill-height"> Holi </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#00E0E0">  </v-card>
             </v-col>
           </v-row>
 
           <v-row style="height: 50%; margin-top: 12px">
             <v-col class="ma-0 pb-0">
-              <v-card class="fill-height"> Hola </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#E8E8E8;">   </v-card>
             </v-col>
 
             <v-col class="ma-0 pb-0">
-              <v-card class="fill-height"> Hola </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#E8E8E8;">  </v-card>
             </v-col>
           </v-row>
         </v-col>
-
-        <v-col>
-          <v-card class="fill-height"> Hola </v-card>
+        <v-col  
+          style="height: 300px; width:170px; min-height:300px; min-width:170px;">
+          
+          <v-card 
+            class="fill-height" 
+            style="background-color:#CCAB87;">  </v-card>
         </v-col>
 
-        <v-col class="pb-0">
+        <v-col 
+          class="pb-0" 
+          style="height: 300px; width:200px; min-height:300px; min-width:200px;">
+
           <v-row style="height: 50%">
             <v-col class="ma-0">
-              <v-card class="fill-height"> Holi </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#00E0E0">  </v-card>
             </v-col>
           </v-row>
 
           <v-row style="height: 50%; margin-top: 12px">
             <v-col class="ma-0 pb-0">
-              <v-card class="fill-height"> Hola </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#E8E8E8;">   </v-card>
             </v-col>
 
             <v-col class="ma-0 pb-0">
-              <v-card class="fill-height"> Hola </v-card>
+              <v-card 
+                class="fill-height"
+                style="background-color:#E8E8E8;">  </v-card>
             </v-col>
           </v-row>
         </v-col>
       </v-row>
 
-      <v-row justify="center">
-        <v-col cols="1" align-self="center">
+      <v-row justify="center" >
+        <v-col 
+          v-for="skill in skills" :key="skill.titulo"
+          cols="1" 
+          align-self="center"  
+          style="min-width:75px;">
           <v-img
-          class="mx-auto"
+            class="mx-auto"
             width="75%"
             contain
             aspect-ratio="1"
-            src="@/assets/logos/html5.svg"
+            :src= "require(`@/assets/logos/${skill.fichero}`)"  
           ></v-img>
         </v-col>
-
-        <v-col cols="1">
-          <v-img contain aspect-ratio="1" src="@/assets/logos/css3.svg"></v-img>
-        </v-col>
-
-        <v-col cols="1">
-          <v-img
-            contain
-            aspect-ratio="1"
-            src="@/assets/logos/javascript.svg"
-          ></v-img>
-        </v-col>
-
-        <v-col cols="1">
-          <v-img
-            contain
-            aspect-ratio="1"
-            src="@/assets/logos/vuejs.svg"
-          ></v-img>
-        </v-col>
-
-        <v-col cols="1">
-          <v-img
-            contain
-            aspect-ratio="1"
-            src="@/assets/logos/photoshop.svg"
-          ></v-img>
-        </v-col>
-
-        <v-col cols="1">
-          <v-img
-            contain
-            aspect-ratio="1"
-            src="@/assets/logos/illustrator.svg"
-          ></v-img>
-        </v-col>
+         
       </v-row>
     </v-container>
   </v-app>
@@ -115,7 +109,38 @@ export default {
   name: "App",
 
   data() {
-    return {};
+    return {
+      skills:[
+        {
+          titulo: 'HTML5',
+          fichero:  'html5.svg'
+        },
+        {
+          titulo: 'CSS3',
+          fichero:  'css3.svg'
+        },
+        {
+          titulo: 'JavaScript',
+          fichero:  'javascript.svg'
+        },
+        {
+          titulo: 'Laravel',
+          fichero:  'laravel.svg'
+        },
+        {
+          titulo: 'Vue',
+          fichero:  'vuejs.svg'
+        },
+        {
+          titulo: 'Photoshop',
+          fichero:  'photoshop.svg'
+        },
+        {
+          titulo: 'Illustrator',
+          fichero:  'illustrator.svg'
+        }, 
+      ]
+    };
   },
 };
 </script>
