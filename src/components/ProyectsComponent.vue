5156a0f3101d9f9bcb2337a01ef60f4a68ad53ae
Oscar <oscarcl.work@gmail.com>
2022-06-01 12:49:09 +0200

    Landing page divided in components

---
+++ src/components/ProyectsComponent.vue
@@ -0,0 +1,144 @@
+<template>
+  <v-container>
+      <v-row class="mt-4">
+        <v-col  
+          style="height: 276px; width:276px; min-height:276px; min-width:276px;">
+          
+          <v-hover >
+            <v-card  
+              class="fill-height d-flex flex-column  " 
+              style="background-color:#CCAB87;"> 
+              <v-spacer></v-spacer> 
+              <v-card-actions >
+                <v-btn icon right > 
+                  <v-icon  >mdi-github</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-hover>
+        </v-col>
+
+        <v-col 
+          class="pb-0" 
+          style="height: 276px; width:276px; min-height:276px; min-width:276px;">
+
+          <v-row style="height: 50%">
+            <v-col class="ma-0">
+              <v-card 
+                class="fill-height d-flex flex-column"
+                style="background-color:#00E0E0"> 
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+
+          <v-row style="height: 50%; margin-top: 12px">
+            <v-col class="ma-0 pb-0 ">
+              <v-card 
+                class="fill-height  d-flex flex-column"
+                style="background-color:#E8E8E8;">   
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>  
+              </v-card>
+            </v-col>
+
+            <v-col class="ma-0 pb-0">
+              <v-card 
+                class="fill-height  d-flex flex-column"
+                style="background-color:#E8E8E8;">   
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>  
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+
+        <v-col  
+          style="height: 276px; width:276px; min-height:276px; min-width:276px;">
+          
+          <v-card 
+            class="fill-height d-flex flex-column" 
+            style="background-color:#CCAB87;">  
+            <v-spacer></v-spacer>
+            <v-card-actions >
+              <v-btn icon right> 
+                <v-icon>mdi-github</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col 
+          class="pb-0" 
+          style="height: 276px; width:276px; min-height:276px; min-width:276px;">
+
+          <v-row style="height: 50%">
+            <v-col class="ma-0">
+              <v-card 
+                class="fill-height d-flex flex-column"
+                style="background-color:#00E0E0"> 
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+
+          <v-row style="height: 50%; margin-top: 12px">
+            <v-col class="ma-0 pb-0 ">
+              <v-card 
+                class="fill-height  d-flex flex-column"
+                style="background-color:#E8E8E8;">   
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>  
+              </v-card>
+            </v-col>
+
+            <v-col class="ma-0 pb-0">
+              <v-card 
+                class="fill-height  d-flex flex-column"
+                style="background-color:#E8E8E8;">   
+                <v-spacer></v-spacer>
+                <v-card-actions >
+                  <v-btn icon right> 
+                    <v-icon>mdi-github</v-icon>
+                  </v-btn>
+                </v-card-actions>  
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+ 
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ProjectsComponent"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/SkillsComponent.vue
@@ -0,0 +1,112 @@
+<template>
+    <v-container>
+        <!--FRONTEND-->
+        <v-row justify="center" class="mt-4">
+            <v-col 
+            v-for="skill in skillsFRONT" :key="skill.titulo"
+            cols="1" 
+            align-self="center"  
+            style="min-width:75px;">
+            
+                <v-img
+                class="mx-auto"
+                width="75%"
+                contain
+                aspect-ratio="1"
+                :src= "require(`@/assets/logos/${skill.fichero}`)"  
+                ></v-img>
+             
+            <h5 class="mt-1" style="wdth:100%;text-align:center;">{{skill.titulo}}</h5>
+            </v-col> 
+        </v-row>
+
+        <!--BACKEND-->
+        <v-row justify="center" >
+            <v-col 
+            v-for="skill in skillsBACK" :key="skill.titulo"
+            cols="1" 
+            align-self="center"  
+            style="min-width:75px;">
+            <v-img
+                class="mx-auto"
+                width="75%"
+                contain
+                aspect-ratio="1"
+                :src= "require(`@/assets/logos/${skill.fichero}`)"  
+            ></v-img>
+            <h5 class="mt-1" style="wdth:100%;text-align:center;">{{skill.titulo}}</h5>
+            </v-col> 
+        </v-row>
+    </v-container> 
+</template>
+
+<script>
+export default {
+    name: "SkillsComponent",
+
+    data() {
+        return {
+        BASE_URL: process.env.BASE_URL, 
+
+        skillsFRONT:[
+            {
+            titulo: 'HTML5',
+            fichero:  'html5.svg'
+            },
+            {
+            titulo: 'CSS3',
+            fichero:  'css3.svg'
+            },
+            {
+            titulo: 'JavaScript',
+            fichero:  'javascript.svg'
+            },
+            {
+            titulo: 'Laravel',
+            fichero:  'laravel.svg'
+            },
+            {
+            titulo: 'Vue',
+            fichero:  'vuejs.svg'
+            },
+            {
+            titulo: 'Photoshop',
+            fichero:  'photoshop.svg'
+            },
+            {
+            titulo: 'Illustrator',
+            fichero:  'illustrator.svg'
+            }, 
+        ],
+
+        skillsBACK:[
+            {
+            titulo: 'Java',
+            fichero:  'java.svg'
+            },
+            {
+            titulo: 'C++',
+            fichero:  'cpp.svg'
+            },
+            {
+            titulo:'NodeJS',
+            fichero: 'nodejs.svg'
+            },
+            {
+            titulo:'MySQL',
+            fichero: 'mysql.svg'
+            },
+            {
+            titulo:'mongoDB',
+            fichero: 'mongodb.svg'
+            } 
+        ],  
+        };
+    },
+};
+ 
+</script>
+
+<style>
+
+</style>
