eb78da7c200d4ab4a05811ebd896ceb69e984e48
Oscar <oscarcl.work@gmail.com>
2022-06-11 22:05:19 +0200

    project component designed

--- src/components/ProyectsComponent.vue
+++ src/components/ProyectsComponent.vue
@@ -1,18 +1,23 @@
 <template>
-  <v-container>
-    <v-slide-group center-active style="width: 100%;">   
-        
-        
-        <v-slide-item v-for="proyect in proyects" :key="proyect.name"  style="width: 500px;  " class="ma-5"> 
-          <v-card  
-            class="pa-3"  
-            style="border-radius:10px;" 
+  
+ 
+
+  <!-- 
+     <v-row>
+    <v-slide-group center-active show-arrows  >   
+        <v-slide-item 
+          v-for="proyect in proyects" 
+          :key="proyect.name"  
+          style="width: 400px;  " 
+          class="ma-5 works"
+         
           > 
-            <v-img 
-              :src= "require(`@/assets/illustrations/${proyect.image}`)" 
-              eager 
-              style="border-radius:10px;"
-            ></v-img>
+          
+          <v-card  
+         
+           
+          > 
+             
           <v-card-text>
             <v-chip-group
               v-model="selection"
@@ -32,11 +37,82 @@
             </v-card-actions>
           </v-card>
         
-        </v-slide-item> 
-         
- </v-slide-group>
-  </v-container>
+        </v-slide-item>  
+    </v-slide-group>
+    </v-row>
+    -->
+
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      center-active
+      show-arrows
+    >
+      <v-slide-item
+        v-for="proyect in proyects" 
+        :key="proyect.name"  
+        v-slot="{ active, toggle }"
+        style="width: 400px; " 
+      >
+        <v-card 
+          class="ma-4 pa-3"  
+          style="border-radius:10px;" 
+          @click="toggle"
+        >
+           <v-img 
+              :src= "require(`@/assets/illustrations/${proyect.image}`)" 
+              eager 
+              style="border-radius:10px;"
+            ></v-img>
+           
+           <v-card-title class="project-title">{{proyect.name}}</v-card-title>
+            <v-card-text>
+            <v-chip-group  
+            >
+              <v-chip v-for="tech in proyect.technologies" :key="tech.name">
+                {{tech.name }}   
+              </v-chip>  
+            </v-chip-group>
+            <div class="project-description">{{proyect.description}}</div>
+          </v-card-text>
+
+            <v-card-actions >
+              <v-btn 
+                v-if="active"
+                icon right > 
+                <v-icon  >mdi-github</v-icon>
+              </v-btn>
+
+              <v-btn 
+                v-if="!active"
+                icon right disabled> 
+              <v-icon  >mdi-github</v-icon>
+            </v-btn>
+
+            </v-card-actions>
+            
+          
+             
+          
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+
+    
+
+ 
 </template>
+
+<style >
+  .works{
+    justify-content: center;
+    align-items: center;
+  }
+
+  .project-title{
+    padding: 5px 5px 5px 5px !important;
+  }
+</style>
 
 <script>
 export default {
@@ -47,6 +123,7 @@ export default {
       proyects: [
         {
           name: "Logo design",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
           image: 'Logotypes.jpg',
           technologies: [ 
             {name : "Illustrator"}
@@ -54,6 +131,7 @@ export default {
         },
         {
           name: "osso brand",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
           image: 'Inspiration.jpg',
           technologies: [
             {name : "VueJS"},
@@ -64,12 +142,41 @@ export default {
         },
         {
           name: "API REST forum",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
           image: 'Weare.jpg',
           technologies: [
             {name : "NodeJS"},
             {name : "Express"}
           ]
-        }
+        },
+        {
+          name: "API REST forum",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
+          image: 'Weare.jpg',
+          technologies: [
+            {name : "NodeJS"},
+            {name : "Express"}
+          ]
+        },
+        {
+          name: "API REST forum",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
+          image: 'Weare.jpg',
+          technologies: [
+            {name : "NodeJS"},
+            {name : "Express"}
+          ]
+        },
+        {
+          name: "API REST forum",
+          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
+          image: 'Weare.jpg',
+          technologies: [
+            {name : "NodeJS"},
+            {name : "Express"}
+          ]
+        } 
+         
 
       ]
     }
