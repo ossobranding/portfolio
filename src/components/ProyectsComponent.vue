704ea8fe3df53b0bbcdf53d5c9df406473f11a45
Oscar <oscarcl.work@gmail.com>
2022-06-12 00:48:08 +0200

    proyects action buttons

--- src/components/ProyectsComponent.vue
+++ src/components/ProyectsComponent.vue
@@ -1,102 +1,67 @@
-<template>
-  
+<template> 
+
+  <v-slide-group
+    v-model="model"
+    class="pa-4"
+    center-active
+    show-arrows
+  >
+    <v-slide-item
+      class="project-slot"
+      v-for="proyect in proyects" 
+      :key="proyect.name"  
+      v-slot="{ active, toggle }"
  
-
-  <!-- 
-     <v-row>
-    <v-slide-group center-active show-arrows  >   
-        <v-slide-item 
-          v-for="proyect in proyects" 
-          :key="proyect.name"  
-          style="width: 400px;  " 
-          class="ma-5 works"
-         
-          > 
-          
-          <v-card  
-         
-           
-          > 
-             
-          <v-card-text>
-            <v-chip-group
-              v-model="selection"
-              active-class="deep-purple accent-4 white--text"
-              column
-            >
-              <v-chip v-for="tech in proyect.technologies" :key="tech.name">
-                {{tech.name }}   
-              </v-chip>  
-            </v-chip-group>
-          </v-card-text>
-
-            <v-card-actions >
-              <v-btn icon right > 
-                <v-icon  >mdi-github</v-icon>
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        
-        </v-slide-item>  
-    </v-slide-group>
-    </v-row>
-    -->
-
-    <v-slide-group
-      v-model="model"
-      class="pa-4"
-      center-active
-      show-arrows
     >
-      <v-slide-item
-        v-for="proyect in proyects" 
-        :key="proyect.name"  
-        v-slot="{ active, toggle }"
-        style="width: 400px; " 
+      <v-card 
+        class="project-card d-flex flex-column "   
+        @click="toggle"
       >
-        <v-card 
-          class="ma-4 pa-3"  
-          style="border-radius:10px;" 
-          @click="toggle"
-        >
-           <v-img 
-              :src= "require(`@/assets/illustrations/${proyect.image}`)" 
-              eager 
-              style="border-radius:10px;"
-            ></v-img>
+        <v-img 
+          :src= "require(`@/assets/illustrations/${proyect.image}`)" 
+          eager 
+          style="border-radius:10px;"
+        ></v-img>
+        
+        <v-card-title class="project-title">{{proyect.name}}</v-card-title>
+        <v-card-text>
+          <v-chip-group  
+            column
+          >
+            <v-chip v-for="tech in proyect.technologies" :key="tech.name">
+              {{tech.name }}   
+            </v-chip>  
+          </v-chip-group>
+          <div class="project-description">{{proyect.description}}</div>
+        </v-card-text>
+
+        <v-spacer></v-spacer>
+
+        <v-card-actions>
+ <v-btn 
+          class="source-code-button"
+          depressed
            
-           <v-card-title class="project-title">{{proyect.name}}</v-card-title>
-            <v-card-text>
-            <v-chip-group  
-            >
-              <v-chip v-for="tech in proyect.technologies" :key="tech.name">
-                {{tech.name }}   
-              </v-chip>  
-            </v-chip-group>
-            <div class="project-description">{{proyect.description}}</div>
-          </v-card-text>
+          v-if="active"
+            > 
+          <v-icon  light>mdi-code-tags</v-icon>
+        </v-btn>
 
-            <v-card-actions >
-              <v-btn 
-                v-if="active"
-                icon right > 
-                <v-icon  >mdi-github</v-icon>
-              </v-btn>
+        <v-btn 
+          class="source-code-button "
+          v-if="!active"
+            depressed disabled> 
+          <v-icon  light >mdi-code-tags</v-icon>
+        </v-btn> 
 
-              <v-btn 
-                v-if="!active"
-                icon right disabled> 
-              <v-icon  >mdi-github</v-icon>
-            </v-btn>
+        <v-btn  class="state-button"   color="warning">Hola</v-btn>
+        </v-card-actions>
+       
+        
+      </v-card>
 
-            </v-card-actions>
-            
-          
-             
-          
-        </v-card>
-      </v-slide-item>
-    </v-slide-group>
+    </v-slide-item>
+  </v-slide-group>
 
     
 
@@ -109,8 +74,24 @@
     align-items: center;
   }
 
+  .project-slot{
+    width: 400px; 
+  }
+
+  .project-card{ 
+    margin: 20px 20px 20px 20px;
+    padding: 15px 15px 15px 15px;
+    border-radius:10px !important; 
+  }
+
   .project-title{
-    padding: 5px 5px 5px 5px !important;
+    padding: 5px 5px 5px 20px !important;
+    font-size: 30px !important;
+    font-weight: bold !important;
+  }
+  
+  .state-button{
+    width: 80%;
   }
 </style>
 
@@ -123,16 +104,18 @@ export default {
       proyects: [
         {
           name: "Logo design",
-          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
+          description: "Recopilación de logotipos propios ",
           image: 'Logotypes.jpg',
+          state: 'In process', 
           technologies: [ 
             {name : "Illustrator"}
           ] 
         },
         {
-          name: "osso brand",
-          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
+          name: "OSSO brand",
+          description: "Diseño y desarrollo de mi marca personal",
           image: 'Inspiration.jpg',
+          state: 'In process', 
           technologies: [
             {name : "VueJS"},
             {name : "JavaScript"},
@@ -141,43 +124,44 @@ export default {
           ]
         },
         {
-          name: "API REST forum",
+          name: "brind.design's web",
           description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
-          image: 'Weare.jpg',
+          image: 'Inspiration.jpg',
+          state: 'In process', 
           technologies: [
             {name : "NodeJS"},
-            {name : "Express"}
+            {name : "Express"},
+            {name : "JWT"} ,
+            {name : "mongoDB"},
+            {name : "API REST"}
           ]
         },
         {
-          name: "API REST forum",
+          name: "SSL sockets",
           description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
           image: 'Weare.jpg',
+          state: 'In process', 
           technologies: [
-            {name : "NodeJS"},
-            {name : "Express"}
+            {name : "SSL"},
+            {name : "Express"},
+            {name : "JWT"} ,
+            {name : "mongoDB"},
+            {name : "API REST"}
           ]
         },
         {
-          name: "API REST forum",
+          name: "SS1 sockets",
           description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
           image: 'Weare.jpg',
+          state: 'Visit it!', 
           technologies: [
-            {name : "NodeJS"},
-            {name : "Express"}
+            {name : "SSL"},
+            {name : "Express"},
+            {name : "JWT"} ,
+            {name : "mongoDB"},
+            {name : "API REST"}
           ]
-        },
-        {
-          name: "API REST forum",
-          description: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaa a aa a a a aaa",
-          image: 'Weare.jpg',
-          technologies: [
-            {name : "NodeJS"},
-            {name : "Express"}
-          ]
-        } 
-         
-
+        }
       ]
     }
   }
