99f5d4b8ff23c6fc8008aaf4e8407ebb2df71f0f
Oscar <oscarcl.work@gmail.com>
2022-06-12 14:13:05 +0200

    RESTYLED landing page with vuetyper & full width div for next background

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,40 +1,25 @@
 <template>
-  <v-container class="landing-container">
+  <div class="landing-container">
       <div id="content">
-        <h3 class="introduction">
-            <span class="name-intro">HOLA, MI NOMBRE ES </span>   
-            <div class="my-name">
-                OSCAR CASADO
-            </div>
-        </h3>
-        <h3 class="introduction">
-            <span class="brand-intro">Y </span> 
-            <div class="brand-name">
-                OSSO
-            </div> 
-            <span class="brand-intro"> ES MI MARCA PERSONAL </span> 
-        </h3>
-        <div class="roles">
-            <p>Soy desarrollador web full-stack  </p>
-            <p>
-                y diseñador UX/UI
-                <vue-typer  
-                    :text='[ "", "(en proceso)"]'
-                    :repeat='Infinity'
-                    :shuffle='false'
-                    initial-action='typing'
-                    :pre-type-delay='70'
-                    :type-delay='70'
-                    :pre-erase-delay='5000'
-                    :erase-delay='250'
-                    erase-style='select-all'
-                    :erase-on-complete='false'
-                    caret-animation='blink' 
-                ></vue-typer>
-            </p>
-        </div>
+        
+        <vue-typer  
+            :text='[ "OSCAR CASADO", "OSSO"]'
+            :repeat='Infinity'
+            :shuffle='false'
+            initial-action='typing'
+            :pre-type-delay='70'
+            :type-delay='70'
+            :pre-erase-delay='5000'
+            :erase-delay='250'
+            erase-style='select-all'
+            :erase-on-complete='false'
+            caret-animation='blink' 
+        ></vue-typer> 
+        <h1 style="text-align: center;  "> Desarrollador web fullsack</h1>
+        <h1 style="text-align: center;  ">& diseñador gráfico</h1>  
       </div> 
-  </v-container> 
+        
+  </div> 
 </template>
 
 <script>
@@ -49,12 +34,12 @@
 
 <style>
     .landing-container { 
-        background-size: cover; 
-        height: 100%;
-
-        display:flex;
-        justify-content: center;
+        height:100vh;
+        width: 100%;   
+        display: flex;
         align-items: center;
+        justify-content: center;
+        background-color: blue;
     }
 
     .introduction {
@@ -63,13 +48,13 @@
 
     .name-intro, .brand-intro {
         -webkit-text-stroke: 2px #606887;
-        font-size: 40px;
+        font-size: 5vw;
         color: transparent; 
     }
 
     .my-name, .brand-name {
         display: inline-block;
-        font-size: 40px;
+        font-size: 5vw;
         color: #606887;
     }
 
@@ -83,7 +68,7 @@
         color: #606887;
     }
 
-    .custom.char {
-        color:#606887;
+    .custom.char { 
+        font-size: 8vw;
     }
 </style>
